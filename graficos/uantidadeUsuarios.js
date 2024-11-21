5bd72766b1ae9136234326a2a956c658eb0f27b8
Beatriz Jacinto de Almeida <106273381+profBia@users.noreply.github.com>
2024-11-21 09:16:45 -0300

    .

--- graficos/uantidadeUsuarios.js
+++ graficos/uantidadeUsuarios.js
@@ -11,13 +11,22 @@ async function quantidadesUsuarios(){
             x : nomeDasRedes,
             y : quantidadesUsuarios,
             type: 'bar'
+            marker: {
+                color: getCSS('--primary-color')
         }
     ]
+    
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+    }
     
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
     Plotly.newPlot(grafico, data)
 }
+
+
 
 quantidadesUsuarios();
